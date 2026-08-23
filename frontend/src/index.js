230822b3f0a2aa96b8ccbfd4d829b84f51763b2a
ialishbaimran30 <i.alishbaimran30@gmail.com>
2026-08-23 23:13:07 +0500

    Move frontend Google OAuth client ID to environment variable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = "646130636057-pvm4nt85umvm0gl2orld8ku7nbmlcert.apps.googleusercontent.com";
+const CLIENT_ID =
+  process.env.REACT_APP_GOOGLE_CLIENT_ID || "646130636057-pvm4nt85umvm0gl2orld8ku7nbmlcert.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
